da9935695f234858cc0a88bbd926ee2748a7ea7e
pakey <pakey@qq.com>
2014-01-07 18:01:19 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+/***************************************************************
+ *   $Program: MCPHP - Web Application Framework $
+ *    $Author: pakey $
+ *     $Email: Pakey@ptcms.com
+ * $Copyright: 2009 - 2012 Ptcms Studio $
+ *      $Link: http://www.ptcms.com $
+ *   $License: http://www.ptcms.com/service/license.html $
+ *      $Date: 2013-03-04 10:48:10 +0800 (星期一, 04 三月 2013) $
+ *      $File: index.php $
+ *  $Revision: 101 $
+ *      $Desc: 入口文件示例
+ **************************************************************/
+
+//define('APP_DEBUG',true);
+include 'MCPHP/MCPHP.php';
